2faa5d88d0b7abf1d3f0d58df7961e0d67feeb73
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-12-20 19:24:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,7 +136,7 @@ client.on('message', message => {
     }
 });
 client.on('message', message => {
-    var prefix = "$"
+    var prefix = "-"
 var args = message.content.split(" ").slice(1);    
 if(message.content.startsWith(prefix + 'id')) {
 var year = message.author.createdAt.getFullYear()
